7cd2e2bc1c540a354cde5118295ee3addb8d96bc
kodek-sleuth <josekodek2000@gmail.com>
2020-05-03 05:17:14 -0700

    Finishing

--- internal/database/adapter.go
+++ internal/database/adapter.go
@@ -4,11 +4,12 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 
 func PostgresConnection() *gorm.DB {
-	db, err := gorm.Open("postgres","postgres://postgres:kevina52@localhost:5432/Embassy?sslmode=disable")
+	db, err := gorm.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil{
 		logrus.Fatal(err)
 	}
